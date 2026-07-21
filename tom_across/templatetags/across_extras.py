ba065ff5df77fa3e820a131984bce51c24bb96f5
Moira Andrews <moira.lee.andrews@gmail.com>
2026-07-21 15:49:04 -0700

    added vis button link to across

---
+++ tom_across/templatetags/across_extras.py
@@ -0,0 +1,18 @@
+from urllib.parse import urlencode
+
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+DEFAULT_ACROSS_APP_URL = 'app.across.sciencecloud.nasa.gov'
+
+@register.simple_tag
+def across_app_visibility_url(target, hi_res=True):
+    base = getattr(settings, 'ACROSS_APP_URL', DEFAULT_ACROSS_APP_URL)
+    query = urlencode({
+        'ra': f'{target.ra:f}',
+        'dec': f'{target.dec:f}',
+        'hi_res': str(hi_res).lower(),
+    })
+    return f'{base}/visibility-calculator?{query}'
