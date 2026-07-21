0cb22bcded959e4b5dbe2ffc3b48aead5458cc4c
Moira Andrews <moira.lee.andrews@gmail.com>
2026-07-21 13:34:39 -0700

    bugfix

--- tom_across/forms.py
+++ tom_across/forms.py
@@ -6,6 +6,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 WAVELENGTH_TYPE_CHOICES = [
     ('angstrom', 'angstrom'),
     ('nm', 'nm'),
@@ -14,51 +15,33 @@ WAVELENGTH_TYPE_CHOICES = [
 
 
 class ObservationFilterForm(forms.Form):
-    start_date = forms.DateField(
-        required=False,
-        widget=forms.DateInput(attrs={'type': 'date'})
-    )
-    end_date = forms.DateField(
-        required=False,
-        widget=forms.DateInput(attrs={'type': 'date'})
-    )
-    wavelength_type = forms.ChoiceField(
-        required=False,
-        label='Unit',
-        choices=WAVELENGTH_TYPE_CHOICES,
-        widget=forms.Select(attrs={'data-wavelength-unit': ''})
-    )
-    wavelength_min = forms.FloatField(
-        required=False,
-        label='Min',
-        widget=forms.NumberInput(attrs={'step': 'any', 'min': '0', 'placeholder': 'Min'})
-    )
-    wavelength_max = forms.FloatField(
-        required=False,
-        label='Max',
-        widget=forms.NumberInput(attrs={'step': 'any', 'min': '0', 'placeholder': 'Max'})
-    )
-    observation_type = forms.ChoiceField(
-        required=False,
-        label='Observation type',
-        choices=[('Any','Imaging', 'Spectroscopy')]
-    )
+    start_date = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}))
+    end_date = forms.DateField(required=False, widget=forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}))
+    wavelength_type = forms.ChoiceField(required=False, label='Unit', choices=WAVELENGTH_TYPE_CHOICES,
+                                        widget=forms.Select(attrs={'data-wavelength-unit': ''}))
+    wavelength_min = forms.FloatField(required=False, label='Min',
+                                      widget=forms.NumberInput(attrs={'step': 'any', 'min': '0', 'placeholder': 'Min'}))
+    wavelength_max = forms.FloatField(required=False, label='Max',
+                                      widget=forms.NumberInput(attrs={'step': 'any', 'min': '0', 'placeholder': 'Max'}))
+    observation_type = forms.ChoiceField(required=False, label='Observation type',
+                                         choices=[('', 'Any'), ('Imaging', 'Imaging'),
+                                                  ('Spectroscopy', 'Spectroscopy')])
+    cone_radius = forms.FloatField(required=False, label='Cone Radius')
 
     def __init__(self, *args, **kwargs):
+        min_date = kwargs.pop('start_date', None)
+        max_date = kwargs.pop('end_date', None)
+        logger.info(f'min and max date from initial? {min_date} {max_date}')
         super().__init__(*args, **kwargs)
-
-        self.fields['wavelength_type'].widget.attrs['data-previous-unit'] = (
-            self._selected_wavelength_type()
-        )
 
         self.helper = FormHelper()
         self.helper.form_tag = False
         self.helper.disable_csrf = True
         self.helper.layout = Layout(
             Row(
-                Column('start_date', css_class='col-md-6'),
-                Column('end_date', css_class='col-md-6'),
-                css_class='row g-3'
+                Column('start_date', css_class='form-group col-md-6 mb-0'),
+                Column('end_date', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
             ),
             Row(
                 Column('wavelength_type', css_class='col-md-4'),
@@ -67,7 +50,8 @@ class ObservationFilterForm(forms.Form):
                 css_class='row g-3'
             ),
             Row(
-                Column('observation_type', css_class='col-md-8'),
+                Column('observation_type', css_class='col-md-4'),
+                Column('cone_radius', css_class='col-md-4'),
                 Column(
                     HTML(
                         '<button type="submit" class="btn btn-outline-primary w-100">'
@@ -78,12 +62,6 @@ class ObservationFilterForm(forms.Form):
                 css_class='row g-3'
             ),
         )
-
-    def _selected_wavelength_type(self):
-        default = WAVELENGTH_TYPE_CHOICES[0][0]
-        if self.is_bound:
-            return self.data.get('wavelength_type') or default
-        return self.initial.get('wavelength_type') or default
 
     def clean(self):
         cleaned_data = super().clean()
