2285859241bbfd163bbaf3f272b6e0356c3a23db
Moira Andrews <moira.lee.andrews@gmail.com>
2026-07-21 13:32:03 -0700

    bug fix

--- tom_across/forms.py
+++ tom_across/forms.py
@@ -41,7 +41,7 @@ class ObservationFilterForm(forms.Form):
     observation_type = forms.ChoiceField(
         required=False,
         label='Observation type',
-        choices=[('', 'Any')] + [(t.value, t.value.title()) for t in ['Imaging', 'Spectroscopy']]
+        choices=[('Any','Imaging', 'Spectroscopy')]
     )
 
     def __init__(self, *args, **kwargs):
